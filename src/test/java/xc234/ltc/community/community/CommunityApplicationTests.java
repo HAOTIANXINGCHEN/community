7f784b747886541e9b41eaa878f5c2823a4a48ad
HAOTIANXINGCHEN <1098756313@qq.com>
2020-01-03 14:55:45 +0800

    init repo

---
+++ src/test/java/xc234/ltc/community/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package xc234.ltc.community.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
